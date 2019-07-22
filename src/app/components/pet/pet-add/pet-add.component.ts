ded45c5cc493e30a7b03e4753d09f36d888c5202
João VItor Malvestio da Silva <joaovitormalvestio@gmail.com>
2019-07-22 13:21:20 -0300

    Added add pet functionality

--- src/app/components/pet/pet-add/pet-add.component.ts
+++ src/app/components/pet/pet-add/pet-add.component.ts
@@ -1,15 +1,50 @@
+import { PetService } from './../../../services/pet/pet.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-pet-add',
   templateUrl: './pet-add.component.html',
-  styleUrls: ['./pet-add.component.css']
+  styleUrls: ['./pet-add.component.scss']
 })
 export class PetAddComponent implements OnInit {
 
-  constructor() { }
+  petForm: FormGroup;
+  name = '';
+  color = '';
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, private petService: PetService, private formBuilder: FormBuilder) { }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.petService.addPet(this.petForm.value)
+      .subscribe((res: any) => {
+          const id = res._id;
+          this.isLoadingResults = false;
+          this.router.navigate(['/pet-details', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
 
   ngOnInit() {
+    this.petForm = this.formBuilder.group({
+      name : [null, Validators.required],
+      color : [null, Validators.required]
+    });
   }
 
 }
